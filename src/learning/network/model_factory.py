8d29a9a54f15e6b0f6427e60c8df3281ff661acd
Csfalpha <3246269778@qq.com>
2025-09-18 23:32:02 +0800

    add requirements.txt

--- src/learning/network/model_factory.py
+++ src/learning/network/model_factory.py
@@ -14,23 +14,23 @@ from learning.network.model_tcn import Tcn, IMUTransformerWithModality
 
 
 def get_model(window_s=100):
-    # network = IMUTransformerWithModality(
-    #     sub_dim=16,
-    #     nhead=8,
-    #     num_layers=2,
-    #     dim_feedforward=256,
-    #     dropout=0.2,
-    #     output_size=3,
-    #     window_size=window_s,
-    #     enabled_modalities=["acc", "gyro", "rotor_spd"]
-    # )
-    network = Tcn(
-        input_size=10,
+    network = IMUTransformerWithModality(
+        sub_dim=16,
+        nhead=8,
+        num_layers=2,
+        dim_feedforward=256,
+        dropout=0.2,
         output_size=3,
-        num_channels=[64, 64, 128],
-        kernel_size=2,
-        dropout=0.3,
-        activation="GELU",
+        window_size=window_s,
+        enabled_modalities=["acc", "gyro", "rotor_spd"]
     )
+    # network = Tcn(
+    #     input_size=10,
+    #     output_size=3,
+    #     num_channels=[64, 64, 128],
+    #     kernel_size=2,
+    #     dropout=0.3,
+    #     activation="GELU",
+    # )
     return network
 
